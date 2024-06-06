48002d9199e271f1481d787119b3e69a0f8316a3
bartbie <bartbie37@gmail.com>
2024-06-06 20:46:42 +0200

    11 current  2024-06-06 20:46:35  23.11.7202.e7cc61784ddf  6.1.91                          *

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,10 @@
     '';
   };
 
+  fonts.packages = with pkgs; [
+    (nerdfonts.override {fonts = ["JetBrainsMono"];})
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
