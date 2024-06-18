f3224ffe980501406a2520a10427c26a498eb883
bartbie37 <bartbie37@gmail.com>
2024-06-18 16:36:40 +0200

    36 current  2024-06-18 16:36:33  23.11.20240604.a2e1d04   6.1.92                          *

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -2,81 +2,11 @@
   config,
   pkgs,
   ...
-}: let
+} @ inputs: let
   user = "bartbie";
 in {
-  home.username = user;
-  home.homeDirectory = "/home/${user}";
-
-  home.packages = with pkgs; [
-    ## same as systemPackages
-    vim
-    gcc
-    wget
-
-    ## gui
-    discord
-    firefox-devedition
-    spotify
-    telegram-desktop
-
-    ## rest
-
-    # archives
-    p7zip
-    unzip
-    xz
-    zip
-
-    # nix
-    alejandra
-    nix-output-monitor
-
-    # utils
-    bat
-    fastfetch
-    file
-    fzf
-    gawk
-    glow
-    gnused
-    gnutar
-    jq
-    lazygit
-    nnn
-    ripgrep
-    tldr
-    tree
-    which
-    zoxide
-
-    # system call
-    lsof # list open files
-    ltrace # library call monitoring
-    strace # system call monitoring
-
-    # networking
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    btop # replacement of htop/nmon
-    dnsutils # `dig` + `nslookup`
-    iftop # network monitoring
-    iotop # io monitoring
-    ipcalc # it is a calculator for the IPv4/v6 addresses
-    iperf3
-    ldns # replacement of `dig`, it provide the command `drill`
-    mtr # A network diagnostic tool
-    nmap # A utility for network discovery and security auditing
-    socat # replacement of openbsd-netcat
-
-    # system tools
-    ethtool
-    lm_sensors # for `sensors` command
-    pciutils # lspci
-    sysstat
-    usbutils # lsusb
-
-    # misc
-    cowsay
+  imports = [
+    (import ../../common/home.nix (inputs // {inherit user;}))
   ];
 
   programs.git = {
@@ -84,66 +14,4 @@ in {
     userName = "bartbie37";
     userEmail = "bartbie37@gmail.com";
   };
-
-  programs.tmux = {
-    enable = true;
-    extraConfig =
-      # sh
-      ''
-        set -g mouse on
-        set-option -g focus-events on
-        set-option -sg escape-time 10
-        set-option -sa terminal-features ',*:RGB'
-      '';
-  };
-
-  programs.starship = {
-    enable = true;
-    # custom settings
-    # settings = {
-    #   add_newline = false;
-    #   aws.disabled = true;
-    #   gcloud.disabled = true;
-    #   line_break.disabled = true;
-    # };
-  };
-
-  programs.wezterm = {
-    enable = true;
-    extraConfig =
-      # lua
-      ''
-        return {
-            hide_tab_bar_if_only_one_tab = true, -- i never use tabs but it may happen accidentally
-            -- enable_tab_bar = false,
-            window_background_opacity = 0.96,
-            color_scheme = "kanagawabones",
-        };
-      '';
-  };
-
-  programs.fish = {
-    enable = true;
-    interactiveShellInit =
-      # sh
-      ''
-        set fish_greeting # Disable greeting
-      '';
-    shellAliases = {
-      vim = "nvim";
-    };
-  };
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.11";
-
-  # Let home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
