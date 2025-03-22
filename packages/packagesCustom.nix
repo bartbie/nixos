ef242448555188bc4e0c99a90cdec53268b7e874
bartbie <bartbie37@gmail.com>
2025-03-22 18:40:55 +0100

    Fixes build fn in packagesCustom

--- packages/packagesCustom.nix
+++ packages/packagesCustom.nix
@@ -6,15 +6,32 @@
   ...
 } @ inputs: let
   wrapped = let
-    build = module:
-      wrapper-manager.lib.build {
-        inherit pkgs;
-        modules = [module];
-        specialArgs = {flake = self;};
-      };
+    # wrapper around wrapper-manager.lib.build that sets meta.mainProgram correctly
+    build = name: mod: let
+      cfg =
+        (wrapper-manager.lib.eval {
+          inherit pkgs;
+          modules = lib.flatten mod;
+          specialArgs = {flake = self;};
+        })
+        .config;
+    in
+      assert lib.assertMsg (builtins.elem name (builtins.attrNames cfg.wrappers)) "name is not in wrappers!";
+        cfg
+        .build
+        .toplevel
+        .overrideAttrs (final: prev: {
+          pname = name;
+          meta =
+            (prev.meta or {})
+            // {
+              name = final.pname;
+              mainProgram = cfg.wrappers.${name}.executableName;
+            };
+        });
     modules = import ../modules/wrapper-manager/list.nix inputs;
   in
-    builtins.mapAttrs (_: build) modules;
+    builtins.mapAttrs build modules;
 
   scripts = {};
 in
