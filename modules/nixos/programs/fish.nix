f152ccb639d1a3af75b5d8b4d91c0c3f0b432477
bartbie <bartbie37@gmail.com>
2025-03-24 18:55:45 +0100

    Fixes wrong package in modules/nixos/programs/fish

--- modules/nixos/programs/fish.nix
+++ modules/nixos/programs/fish.nix
@@ -7,22 +7,18 @@
 }: let
   inherit (lib) mkEnableOption;
   cfg = config.nixon.programs.fish;
+  package = pkgs.nixon.fish;
 in {
   options.nixon.programs.fish = {
     enable = mkEnableOption "fish";
-    package = lib.mkPackageOption pkgs ["nixon" "fish"] {};
   };
   config = lib.mkIf cfg.enable {
-    programs.fish = {
-      inherit (cfg) package;
-      enable = true;
-    };
     programs.bash = {
       interactiveShellInit = ''
-        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        if [[ $(${lib.getExe' pkgs.procps "ps"} --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
         then
           shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${cfg.package}/bin/fish $LOGIN_OPTION
+          exec ${lib.getExe' package "fish"} $LOGIN_OPTION
         fi
       '';
     };
