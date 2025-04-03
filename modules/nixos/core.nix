30cd83def76043f5823a163d431bba709a9bcf5f
bartbie <bartbie37@gmail.com>
2025-04-04 01:19:08 +0200

    nixos: refactors core

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -8,32 +8,68 @@
 }: let
   inherit (lib) mkEnableOption;
   cfg = config.nixon.core;
+
   tru = lib.mkDefault true;
   fal = lib.mkDefault false;
+
+  mk = b: {
+    disableDarwin ? false,
+    force ? false,
+  } @ args: let
+    state = args // {inherit (pkgs.stdenv) isDarwin;};
+    mkFlag =
+      if force
+      then lib.mkForce
+      else lib.mkDefault;
+  in
+    flake.lib.match state [
+      [
+        {
+          disableDarwin = true;
+          isDarwin = true;
+        }
+        false
+      ]
+      [{} (mkFlag b)]
+      [{} (mkFlag b)]
+    ];
 in {
   imports = flake.lib.findImports ./core.nix [./default.nix];
   options.nixon.core.enable = mkEnableOption "core";
-  config.nixon = lib.mkIf cfg.enable {
-    base.enable = tru;
-    audio = {
-      enable = tru;
-      bluetooth.enable = tru;
-    };
-    boot.enable = tru;
-    fonts.enable = tru;
-    net.enable = tru;
-    nix.enable = tru;
-    ssh.enable = tru;
-    users.enable = tru;
-    wayland.enable = tru;
+  config = {
+    assertions = [
+      {
+        assertion = !(pkgs.stdenv.isDarwin && config.nixon.impernamence.enable);
+        message = "Darwin doesn't support impernamence!";
+      }
+      {
+        assertion = !(pkgs.stdenv.isDarwin && config.nixon.disko.enable);
+        message = "Darwin doesn't support disko!";
+      }
+    ];
 
-    packages.enable = tru;
+    nixon = lib.mkIf cfg.enable {
+      base.enable = tru;
+      audio = {
+        enable = tru;
+        bluetooth.enable = tru;
+      };
+      boot.enable = tru;
+      fonts.enable = tru;
+      net.enable = tru;
+      nix.enable = tru;
+      ssh.enable = tru;
+      users.enable = tru;
+      wayland.enable = mk true {disableDarwin = true;};
 
-    programs = {
-      fish.enable = tru;
-      firefox.enable = tru;
-      plasma5.enable = fal;
-      hypr.enable = tru;
+      packages.enable = tru;
+
+      programs = {
+        fish.enable = mk true {};
+        firefox.enable = mk true {};
+        hypr.enable = mk true {disableDarwin = true;};
+        plasma5.enable = mk false {disableDarwin = true;};
+      };
     };
   };
 }
