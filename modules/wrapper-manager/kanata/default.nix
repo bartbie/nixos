47ef71727578390e8deeffd9f7b521c950c321cc
bartbie <bartbie37@gmail.com>
2025-05-11 22:50:59 +0200

    wrappers: configure kanata

--- modules/wrapper-manager/kanata/default.nix
+++ modules/wrapper-manager/kanata/default.nix
@@ -7,6 +7,8 @@
     arg0 = lib.getExe' pkgs.unstable.kanata "kanata";
     prependArgs = [
       "--cfg"
+      # for live-reload
+      # "/etc/nixos/modules/wrapper-manager/kanata/config.kbd"
       ./config.kbd
     ];
   };
