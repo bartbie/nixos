16573ffcfa1180c1e200942f32e358d50361bff9
bartbie <bartbie37@gmail.com>
2025-05-30 02:24:13 +0200

    nixos/system: adds autologin

--- modules/nixos/system/users.nix
+++ modules/nixos/system/users.nix
@@ -14,6 +14,7 @@ in {
     bartbie = {
       enable = (mkEnableOption "bartbie user") // {default = true;};
       useHashedPasswordFile = (mkEnableOption "hashed password") // {default = true;};
+      autologin = (mkEnableOption "hashed password") // {default = true;};
     };
   };
   config = lib.mkIf cfg.enable {
@@ -53,4 +54,5 @@ in {
       ];
     };
   };
+  services.getty.autologinUser = lib.mkIf cfg.bartbie.autologin "bartbie";
 }
