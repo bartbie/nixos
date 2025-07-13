604582ce874242669095c87ac73d43b2e02ee311
bartbie <bartbie37@gmail.com>
2025-07-13 19:59:19 +0200

    shell: set up devShells correctly for rust

--- packages/shell.nix
+++ packages/shell.nix
@@ -4,64 +4,97 @@
   self,
   ...
 }: let
-  mkShell = name: pkgset:
-    pkgs.mkShell {
-      inherit name;
-      buildInputs = pkgset;
+  rust-toolchain = let
+    toolchain = channel: ver: pkgs.rust-bin.${channel}.${ver}.default;
+  in
+    (toolchain "stable" "latest").override (p: {
+      extensions = p.extensions ++ ["rust-src"];
+    });
+
+  dev-pkgs = let
+    flatten = lib.flip lib.pipe [
+      (self.lib.attrsets.bypath.flattenToListCond (x: !(lib.isDerivation x)))
+      (builtins.map (x: x.value))
+    ];
+  in
+    flatten {
+      common = {
+        inherit
+          (pkgs.nixon)
+          git
+          jujutsu
+          ;
+      };
+      nix = {
+        inherit
+          (pkgs)
+          nil
+          ;
+        fmt = self.formatter.${pkgs.system};
+      };
+      nushell = {
+        inherit
+          (pkgs)
+          nushell
+          ;
+      };
+      rust = {
+        inherit
+          (pkgs)
+          gobject-introspection
+          ;
+        inherit
+          (pkgs.unstable)
+          rust-analyzer
+          ;
+        inherit rust-toolchain;
+      };
     };
-  flatten = lib.flip lib.pipe [
-    (self.lib.attrsets.bypath.flattenToListCond (x: !(lib.isDerivation x)))
-    (builtins.map (x: x.value))
-  ];
-  dev-pkgs = flatten {
-    common = {
-      inherit
-        (pkgs.nixon)
-        git
-        jujutsu
-        ;
-    };
-    nix = {
-      inherit
-        (pkgs)
-        nil
-        ;
-      fmt = self.formatter.${pkgs.system};
-    };
-    nushell = {
-      inherit
-        (pkgs)
-        nushell
-        ;
-    };
-    rust = let
-      toolchain = channel: ver: pkgs.rust-bin.${channel}.${ver}.default;
-    in {
-      inherit
-        (pkgs.unstable)
-        rust-analyzer
-        ;
-      rs = (toolchain "stable" "latest").override (p: {
-        extensions = p.extensions ++ ["rust-src"];
-      });
-    };
-  };
+
+  mkDevShell = args:
+    (pkgs.mkShell {
+      buildInputs = dev-pkgs;
+      nativeBuildInputs = builtins.attrValues {
+        inherit
+          (pkgs)
+          pkg-config
+          ;
+      };
+      RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+    })
+    // args;
 in {
   default = self.devShells.${pkgs.system}.devWithLix;
-  wrapped = mkShell "nixon-wrapped-shell" pkgs.nixon;
-  all = mkShell "nixon-all-shell" (pkgs.nixon // pkgs.systemPackages);
-  dev = mkShell "nixon-dev-shell" dev-pkgs;
-  devWithLix = mkShell "nixon-dev-lix-shell" (dev-pkgs
-    ++ [
-      self.inputs.lix-module.packages.${pkgs.system}.default
-      pkgs.nh
-      pkgs.nixos-rebuild
-    ]);
-  devWithNvim = mkShell "nixon-dev-nvim-shell" (dev-pkgs
-    ++ builtins.attrValues {
-      inherit
-        (pkgs)
-        bartbie-nvim-nightly
-        ;
-    });
+  wrapped = pkgs.mkShell {
+    name = "nixon-wrapped-shell";
+    buildInputs = builtins.attrValues pkgs.nixon;
+  };
+  all = pkgs.mkShell {
+    name = "nixon-all-shell";
+    buildInputs = builtins.attrValues (pkgs.nixon // pkgs.systemPackages);
+  };
+  dev = mkDevShell {
+    name = "nixon-dev-shell";
+  };
+  devWithLix = mkDevShell {
+    name = "nixon-dev-lix-shell";
+    buildInputs =
+      dev-pkgs
+      ++ [
+        self.inputs.lix-module.packages.${pkgs.system}.default
+        pkgs.nh
+        pkgs.nixos-rebuild
+      ];
+  };
+  devWithNvim = mkDevShell {
+    name = "nixon-dev-nvim-shell";
+    buildInputs =
+      dev-pkgs
+      ++ builtins.attrValues {
+        inherit
+          (pkgs)
+          bartbie-nvim-nightly
+          ;
+      };
+  };
 }
