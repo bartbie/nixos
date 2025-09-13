b6b262d584aefa28334341183178a2cd36d5963f
bartbie <bartbie37@gmail.com>
2025-09-13 23:53:25 +0200

    flake: refit packages to new arch

--- modules/A-packages.nix
+++ modules/A-packages.nix
@@ -1,145 +1,113 @@
-{lib, ...}: let
-  common = pkgs: {
-    inherit
-      (pkgs)
-      nvim
-      gcc
-      vim
-      wget
-      # archives
-      p7zip
-      unzip
-      xz
-      zip
-      # utils
-      file
-      fd
-      fzf
-      gawk
-      gnused
-      gnutar
-      jq
-      ripgrep
-      tldr
-      tokei
-      which
-      ipcalc # it is a calculator for the IPv4/v6 addresses
-      # system call
-      lsof # list open files
-      ;
-  };
+let
+  mk = l: {add = l;};
+in {
+  packages = {
+    generic = {
+      base = {pkgs, ...}:
+        mk {
+          inherit
+            (pkgs)
+            gcc
+            vim
+            wget
+            # archives
+            p7zip
+            unzip
+            xz
+            zip
+            # utils
+            file
+            fd
+            fzf
+            gawk
+            gnused
+            gnutar
+            jq
+            ripgrep
+            tldr
+            tokei
+            which
+            ipcalc # it is a calculator for the IPv4/v6 addresses
+            # system call
+            lsof # list open files
+            ;
+        };
+      pc = {
+        pkgs,
+        pkgs-unstable,
+        ...
+      }:
+        mk {
+          inherit
+            (pkgs-unstable)
+            ;
+          inherit
+            (pkgs)
+            ## gui
+            discord-canary
+            spotify
+            telegram-desktop
+            ## rest
+            fastfetch
+            ffmpeg
+            lazygit
+            glow
+            zoxide
+            # nix
+            alejandra
+            nix-output-monitor
+            nnn
+            # networking
+            aria2 # A lightweight multi-protocol & multi-source command-line download utility
+            btop # replacement of htop/nmon
+            dnsutils # `dig` + `nslookup`
+            iftop # network monitoring
+            iperf3
+            ldns # replacement of `dig`, it provide the command `drill`
+            mtr # A network diagnostic tool
+            nmap # A utility for network discovery and security auditing
+            socat # replacement of openbsd-netcat
 
-  perClass = pkgs: {
+            # misc
+            cowsay
+            # langs
+            rustup
+            ;
+        };
+    };
     nixos = {
-      inherit
-        (pkgs)
-        ltrace # library call monitoring
-        strace # system call monitoring
-        ;
+      base = {pkgs, ...}:
+        mk {
+          inherit
+            (pkgs)
+            ltrace # library call monitoring
+            strace # system call monitoring
+            ;
+        };
+      server = {pkgs, ...}: mk {};
+      pc = {pkgs, ...}:
+        mk {
+          inherit
+            (pkgs)
+            # clipboard
+            wl-clipboard
+            clipse
+            # gui
+            qimgv
+            # utils
+            # system tools
+            sysstat
+            usbutils # lsusb
+            ethtool
+            lm_sensors # for `sensors` command
+            pciutils # lspci
+            iotop # io monitoring
+            ;
+        };
     };
 
     darwin = {
-      inherit
-        (pkgs)
-        ;
+      pc = {pkgs, ...}: mk {inherit (pkgs);};
     };
-  };
-
-  perTag = pkgs: {
-    pc = {
-      inherit
-        (pkgs.unstable)
-        ;
-      inherit
-        (pkgs)
-        ## gui
-        discord-canary
-        spotify
-        telegram-desktop
-        ## rest
-        fastfetch
-        ffmpeg
-        lazygit
-        glow
-        zoxide
-        # nix
-        alejandra
-        nix-output-monitor
-        nnn
-        # networking
-        aria2 # A lightweight multi-protocol & multi-source command-line download utility
-        btop # replacement of htop/nmon
-        dnsutils # `dig` + `nslookup`
-        iftop # network monitoring
-        iperf3
-        ldns # replacement of `dig`, it provide the command `drill`
-        mtr # A network diagnostic tool
-        nmap # A utility for network discovery and security auditing
-        socat # replacement of openbsd-netcat
-
-        # misc
-        cowsay
-        # langs
-        rustup
-        ;
-    };
-    server = {};
-  };
-
-  nixos-pc = pkgs: {
-    inherit
-      (pkgs)
-      # clipboard
-      wl-clipboard
-      clipse
-      # gui
-      qimgv
-      # utils
-      # system tools
-      sysstat
-      usbutils # lsusb
-      ethtool
-      lm_sensors # for `sensors` command
-      pciutils # lspci
-      iotop # io monitoring
-      ;
-  };
-
-  concatPkgs = pkgs: fns:
-    fns
-    |> builtins.map (fn: fn pkgs)
-    |> builtins.map builtins.attrValues
-    |> lib.concatLists;
-
-  switch = name: attrsFn: pkgs: (attrsFn pkgs).${name} or {};
-
-  mapTags = tags: builtins.map (t: switch t perTag) tags;
-
-  mkPkgs = config: pkgs: rest: concatPkgs pkgs ([(mapTags config.meta.tags) (switch config.meta.class perClass) common] ++ rest);
-  mkMod = rest: ({
-    pkgs,
-    config,
-    ...
-  }: {
-    environment.systemPackages = mkPkgs pkgs config rest;
-  });
-in {
-  flake.modules = {
-    nixos = {
-      pc = mkMod [nixos-pc];
-      server = mkMod [];
-    };
-    darwin.base = mkMod [];
   };
 }
-#TODO
-#   wrapper-manager = {
-#     packages = flake.wrapperManagerModules.list.base;
-#     sharedModules = [flake.wrapperManagerModules.options];
-#     extraSpecialArgs = {
-#       inherit flake;
-#       inherit (flake.lib) theme;
-#     };
-#     enableInstall = cfg.wrapped.enable;
-#   };
-
---
+++ modules/A-scripts.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  nixonLib,
+  ...
+}: let
+  mkScripts = pkgs: let
+    inherit (pkgs) writers;
+    mkBash = script: writers.writeBashBin script (builtins.readFile (nixonLib.scriptsPath + /${script}.sh));
+    mkNu = script: writers.writeNuBin script (builtins.readFile (nixonLib.scriptsPath + /${script}.nu));
+  in {
+    # home-export = mkBash "home-export";
+    rand-wp = mkBash "rand-wp";
+    rebuild = mkNu "rebuild";
+  };
+in {
+  hosts.shared = {pkgs, ...}: {
+    environment.systemPackages = builtins.attrValues (mkScripts pkgs);
+  };
+  perSystem = {pkgs, ...}: {
+    packages = mkScripts pkgs;
+  };
+}
