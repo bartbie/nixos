2a45427401e411ef3acabf43c07ec38cd694acea
bartbie <bartbie37@gmail.com>
2025-12-10 21:44:12 +0100

    flake: Redo packages mechanism to map every set to appropriate module

--- modules/A-internal/packages.nix
+++ modules/A-internal/packages.nix
@@ -5,7 +5,7 @@
 }: let
   inherit (lib) types;
 
-  scaffolding = {config, ...}: {
+  optionsModule = {config, ...}: {
     options = {
       add = lib.mkOption {
         type = lib.types.coercedTo (types.attrsOf types.package) builtins.attrValues (types.listOf types.package);
@@ -13,6 +13,24 @@
       };
     };
   };
+
+  eval = mod: {
+    pkgs,
+    pkgs-unstable,
+    nixonArgs,
+    ...
+  }: (
+    if mod == {}
+    then []
+    else
+      (lib.evalModules {
+        modules = [
+          mod
+          optionsModule
+          {_module.args = {inherit pkgs pkgs-unstable nixonArgs;} // nixonArgs;}
+        ];
+      }).config.add
+  );
 in {
   options = {
     packages = lib.mkOption {
@@ -25,25 +43,50 @@ in {
       default = {};
     };
   };
+
+  # Add packages.generic to systemPackages filtered by systems' tags
+  # This avoids flake.modules.generic.X from host-environment modules anti-pattern
+  config.hosts.shared.imports =
+    config.packages.generic
+    |> lib.mapAttrsToList (
+      tag: module: (
+        # SAFETY:
+        # need to request args explicitly from module system
+        ({
+            pkgs,
+            pkgs-unstable,
+            nixonArgs,
+            ...
+          } @ args: let
+            config' = args.config;
+          in {
+            # TODO: make base tag attached to everyone be default
+            environment.systemPackages = lib.mkIf (tag == "base" || builtins.elem tag config'.meta.tags) (
+              eval module args
+            );
+          })
+      )
+    );
+
+  # Create package module for each class and its declared tag
+  # packages {<class> { <name> <module> }}
   config.flake.modules =
     config.packages
     |> (x: builtins.removeAttrs x ["generic"])
-    |> builtins.mapAttrs (_class: per-tag:
-      per-tag
-      |> builtins.mapAttrs (modName: pkgSubmodule: (
-        {config, ...}: let
-          eval = mod:
-            if mod == {}
-            then []
-            else
-              (lib.evalModules {
-                specialArgs = builtins.removeAttrs config._module.args ["config" "options"];
-                modules = [scaffolding mod];
-              }).config.add;
-        in {
-          environment.systemPackages =
-            (eval pkgSubmodule)
-            ++ (eval (config.packages.generic.${modName} or {}));
-        }
-      )));
+    |> builtins.mapAttrs (_class: tagged-modules:
+      tagged-modules
+      # |> (x: x // {base = x.base or {};})
+      |> builtins.mapAttrs (
+        _tag: module:
+        # SAFETY:
+        # need to request args explicitly from module system
+        ({
+            pkgs,
+            pkgs-unstable,
+            nixonArgs,
+            ...
+          } @ args: {
+            environment.systemPackages = eval module args;
+          })
+      ));
 }
