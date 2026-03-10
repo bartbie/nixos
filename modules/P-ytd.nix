6bcf2207b51449c25061277e412603a98d208fde
bartbie <bartbie37@gmail.com>
2026-03-10 15:31:49 +0100

    wrappers: ytd quickfix

--- modules/P-ytd.nix
+++ modules/P-ytd.nix
@@ -3,7 +3,7 @@
   wrapped.mpv.module =
     { pkgs-unstable, ... }:
     {
-      wrappers = nixonLib.pkgh.mapArg0 pkgs-unstable.yt-dpl "yt-dpl" {
+      wrappers = nixonLib.pkgh.mapArg0 pkgs-unstable.yt-dlp "yt-dlp" {
         ytd-audio.prependArgs = [
           "-t"
           "mp3"
