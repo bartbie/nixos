df9bcedd178e2fa696af6f35adff42e4910b22ef
bartbie <bartbie37@gmail.com>
2026-04-18 13:16:43 +0200

    lyndon: acpi kernel param lax

--- modules/A-hosts/lyndon/led.nix
+++ modules/A-hosts/lyndon/led.nix
@@ -4,6 +4,7 @@
     { config, pkgs-unstable, ... }:
     {
       hardware.i2c.enable = true;
+      boot.kernelParams = [ "acpi_enforce_resources=lax" ];
       services.hardware.openrgb = {
         enable = true;
         package = pkgs-unstable.openrgb-with-all-plugins;
